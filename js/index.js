2a0daf7bf38018f00633f50ef981b19acd4dd469
rouwe <santosroweme14@gmail.com>
2022-01-19 10:46:48 +0800

    Update path

--- js/index.js
+++ js/index.js
@@ -138,7 +138,7 @@ function reinitSliderSrc() {
 <<<<<<< HEAD
     heroSlider.style.background = `url(room_homepage/images/${sliderSrc[1]}.jpg)`;   
 =======
-    heroSlider.style.background = `url(https://github.com/rouwe/room_homepage/blob/main/images/${sliderSrc[1]}.jpg)`;   
+    heroSlider.style.background = `url(room_homepage/blob/main/images/${sliderSrc[1]}.jpg)`;   
 >>>>>>> 828a3a2cb53e5f2d2896230e33fe37658a183544
     modifiedSliderAddStyle(heroSlider); // Additional style for slider image
 }
